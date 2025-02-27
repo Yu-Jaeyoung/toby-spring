56923a757bf414cdabc472cd9fb8638df7f19696
Yu-Jaeyoung <jackyu0721@gmail.com>
2025-02-27 16:53:59 +0900

    서브클래스에서 구체적인 오브젝트 생성 방법을 결정 / 팩토리 메소드 패턴

---
+++ src/springbook/user/dao/DUserDao.java
@@ -0,0 +1,16 @@
+package springbook.user.dao;
+
+import springbook.user.conf.Conf;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DUserDao extends UserDao {
+
+    @Override
+    public Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("org.postgresql.Driver");
+        return DriverManager.getConnection(Conf.DB_URL, Conf.DB_USER, Conf.DB_PASSWORD);
+    }
+}
