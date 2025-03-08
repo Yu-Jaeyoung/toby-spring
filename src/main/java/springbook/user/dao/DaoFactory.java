377859609e62333cff5e9e813b55bb073434b5a1
Yu-Jaeyoung <jackyu0721@gmail.com>
2025-03-08 15:08:27 +0900

    DataSource 타입의 dataSource 빈 정의 메소드, DI를 받는 userDao() 빈 정의 메소드

--- src/main/java/springbook/user/dao/DaoFactory.java
+++ src/main/java/springbook/user/dao/DaoFactory.java
@@ -2,17 +2,30 @@ package springbook.user.dao;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import springbook.user.conf.Conf;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DaoFactory {
 
     @Bean
     public UserDao userDao() {
-        return new UserDao(connectionMaker());
+        UserDao userDao = new UserDao();
+        userDao.setDataSource(dataSource());
+        return userDao;
     }
 
     @Bean
-    public ConnectionMaker connectionMaker() {
-        return new DConnectionMaker();
+    public DataSource dataSource() {
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+
+        dataSource.setDriverClass(org.postgresql.Driver.class);
+        dataSource.setUrl(Conf.DB_URL);
+        dataSource.setUsername(Conf.DB_USER);
+        dataSource.setPassword(Conf.DB_PASSWORD);
+
+        return dataSource;
     }
 }
