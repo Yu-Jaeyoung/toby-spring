ffc7aab05951b335088a7a7073ee31ca52b984e1
Yu-Jaeyoung <jackyu0721@gmail.com>
2025-03-09 15:56:28 +0900

    UserService의 테스트용 대역 클래스, 테스트용 예외 생성

--- src/test/java/springbook/user/service/UserServiceTest.java
+++ src/test/java/springbook/user/service/UserServiceTest.java
@@ -29,6 +29,23 @@ class UserServiceTest {
 
     List<User> users;
 
+    static class TestUserService extends UserService {
+        private String id;
+
+        private TestUserService(String id) {
+            this.id = id;
+        }
+
+        @Override
+        protected void upgradeLevel(User user) {
+            if (user.getId().equals(this.id)) throw new TestUserServiceException();
+            super.upgradeLevel(user);
+        }
+    }
+
+    static class TestUserServiceException extends RuntimeException {
+
+    }
 
     @BeforeEach
     void setUp() {
