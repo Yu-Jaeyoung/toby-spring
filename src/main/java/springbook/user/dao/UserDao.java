4b59902179b43a9e0d7df06f51b856715d0df973
Yu-Jaeyoung <jackyu0721@gmail.com>
2025-03-09 13:32:06 +0900

    기술에 독립적인 UserDao 만들기
    - UserDao 클래스 명칭을 UserDaoJdbc로 변경
    - UserDao 인터페이스 생성
    - UserDaoJdbc가 UserDao 인터페이스를 구현하도록 선언
    - ApplicationContext의 빈 클래스 변경

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -1,68 +1,17 @@
 package springbook.user.dao;
 
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import springbook.user.domain.User;
 
-import javax.sql.DataSource;
-import java.sql.*;
 import java.util.List;
 
-public class UserDao {
-    private JdbcTemplate jdbcTemplate;
-    private RowMapper<User> userMapper = new RowMapper<User>() {
-        @Override
-        public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-            User user = new User();
-            user.setId(rs.getString("id"));
-            user.setName(rs.getString("name"));
-            user.setPassword(rs.getString("password"));
+public interface UserDao {
+    void add(User user);
 
-            return user;
-        }
-    };
+    User get(String id);
 
-    public UserDao() {
-    }
+    List<User> getAll();
 
-    public void setDataSource(DataSource dataSource) {
-        this.jdbcTemplate = new JdbcTemplate(dataSource);
-    }
+    void deleteAll();
 
-    public void add(final User user) {
-        this.jdbcTemplate.update(
-                "insert into users(id, name, password) values (? ,?, ?)",
-                user.getId(), user.getName(), user.getPassword()
-        );
-    }
-
-    public User get(String id) {
-        return this.jdbcTemplate.queryForObject(
-                "select * from users where id = ?",
-                new Object[]{id},
-                this.userMapper
-        );
-    }
-
-    public void deleteAll() {
-        this.jdbcTemplate.update("delete from users");
-    }
-
-    private static PreparedStatement makeStatement(Connection c) throws SQLException {
-        return c.prepareStatement("delete from users");
-    }
-
-    public int getCount() {
-        return this.jdbcTemplate.queryForObject(
-                "select count(*) from users",
-                Integer.class
-        );
-    }
-
-    public List<User> getAll() {
-        return this.jdbcTemplate.query(
-                "select * from users order by id",
-                this.userMapper
-        );
-    }
+    int getCount();
 }
