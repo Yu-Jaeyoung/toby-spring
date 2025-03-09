43d40320e4948f5266cc5c663fe50b16bcbaca3f
Yu-Jaeyoung <jackyu0721@gmail.com>
2025-03-09 15:41:13 +0900

    UserService에 DI를 통해 UserLevelUpgradePolicy 적용

--- src/main/java/springbook/user/service/UserService.java
+++ src/main/java/springbook/user/service/UserService.java
@@ -8,40 +8,24 @@ import java.util.List;
 
 public class UserService {
     UserDao userDao;
-
-    public static final int MIN_LOGCOUNT_FOR_SILVER = 50;
-    public static final int MIN_RECCOMEND_FOR_GOLD = 30;
+    UserLevelUpgradePolicy userLevelUpgradePolicy;
 
     public void setUserDao(UserDao userDao) {
         this.userDao = userDao;
     }
 
+    public void setUserLevelUpgradePolicy(UserLevelUpgradePolicy userLevelUpgradePolicy) {
+        this.userLevelUpgradePolicy = userLevelUpgradePolicy;
+    }
+
     public void upgradeLevels() {
         List<User> users = userDao.getAll();
         for (User user : users) {
-            if (canUpgradeLevel(user)) {
-                upgradeLevel(user);
+            if (userLevelUpgradePolicy.canUpgradeLevel(user)) {
+                userLevelUpgradePolicy.upgradeLevel(user);
+                userDao.update(user);
             }
         }
-    }
-
-    private boolean canUpgradeLevel(User user) {
-        Level currentLevel = user.getLevel();
-        switch (currentLevel) {
-            case BASIC:
-                return (user.getLogin() >= MIN_LOGCOUNT_FOR_SILVER);
-            case SILVER:
-                return (user.getRecommend() >= MIN_RECCOMEND_FOR_GOLD);
-            case GOLD:
-                return false;
-            default:
-                throw new IllegalArgumentException("Unknown Level: " + currentLevel);
-        }
-    }
-
-    private void upgradeLevel(User user) {
-        user.upgradeLevel();
-        userDao.update(user);
     }
 
     public void add(User user) {
