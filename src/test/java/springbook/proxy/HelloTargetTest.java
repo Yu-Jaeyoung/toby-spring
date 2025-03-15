e0f94d5e96d648ef756b32780f8fcdb43ec72bdd
Yu-Jaeyoung <jackyu0721@gmail.com>
2025-03-15 22:49:12 +0900

    프록시를 적용할 타깃 클래스, 클라이언트 역할의 테스트

---
+++ src/test/java/springbook/proxy/HelloTargetTest.java
@@ -0,0 +1,18 @@
+package springbook.proxy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class HelloTargetTest {
+    @Test
+    public void simpleProxy() {
+        Hello hello = new HelloTarget(); // 타깃은 인터페이스를 통해 접근하는 습관을 들여야함
+
+        assertThat(hello.sayHello("Toby"), is("Hello Toby"));
+        assertThat(hello.sayHi("Toby"), is("Hi Toby"));
+        assertThat(hello.sayThankYou("Toby"), is("Thank you Toby"));
+
+    }
+}
