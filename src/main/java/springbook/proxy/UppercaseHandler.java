a5ac8366ff50a654d717451130d294f5c9ac794f
Yu-Jaeyoung <jackyu0721@gmail.com>
2025-03-16 13:41:25 +0900

    메소드를 선별해서 부가기능을 적용하는 invoke()

--- src/main/java/springbook/proxy/UppercaseHandler.java
+++ src/main/java/springbook/proxy/UppercaseHandler.java
@@ -7,7 +7,6 @@ public class UppercaseHandler implements InvocationHandler {
 
     Object target;
 
-    // 어떤 종류의 인터페이스를 구현한 타깃에도 적용 가능하도록 Object 타입으로 수정
     public UppercaseHandler(Object target) {
         this.target = target;
     }
@@ -16,8 +15,8 @@ public class UppercaseHandler implements InvocationHandler {
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         Object ret = method.invoke(target, args);
 
-        // 호출된 메소드의 리턴 타입이 String 인 경우만 대문자 변경 기능을 적용하도록 수정
-        if (ret instanceof String) {
+        // 리턴 타입과 메소드 이름이 일치하는 경우에만 부가기능을 제공
+        if (ret instanceof String && method.getName().startsWith("say")) {
             return ((String) ret).toUpperCase();
         }
 
