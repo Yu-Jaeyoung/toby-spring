ec27df2fc00597885327fd48fced26ee83e228d4
Yu-Jaeyoung <jackyu0721@gmail.com>
2025-03-16 17:03:02 +0900

    스프링 ProxyFactoryBean을 이용한 다이내믹 프록시 테스트

---
+++ src/test/java/springbook/learningtest/jdk/proxy/DynamicProxyTest.java
@@ -0,0 +1,51 @@
+package springbook.learningtest.jdk.proxy;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.framework.ProxyFactoryBean;
+import springbook.proxy.Hello;
+import springbook.proxy.HelloTarget;
+import springbook.proxy.UppercaseHandler;
+
+import java.lang.reflect.Proxy;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class DynamicProxyTest {
+    @Test
+    void simpleProxy() {
+        Hello proxiedHello = (Hello) Proxy.newProxyInstance(
+                getClass().getClassLoader(),
+                new Class[]{ Hello.class },
+                new UppercaseHandler(new HelloTarget())
+        );
+
+        assertThat(proxiedHello.sayHello("Toby"), is("HELLO TOBY"));
+        assertThat(proxiedHello.sayHi("Toby"), is("HI TOBY"));
+        assertThat(proxiedHello.sayThankYou("Toby"), is("THANK YOU TOBY"));
+    }
+
+    @Test
+    void proxyFactoryBean() {
+        ProxyFactoryBean pfBean = new ProxyFactoryBean();
+        pfBean.setTarget(new HelloTarget());
+        pfBean.addAdvice(new UppercaseAdvice()); // 부가 기능을 담은 어드바이스를 추가 (여러 개 가능)
+
+        Hello proxiedHello = (Hello) pfBean.getObject();
+        assertThat(proxiedHello.sayHello("Toby"), is("HELLO TOBY"));
+        assertThat(proxiedHello.sayHi("Toby"), is("HI TOBY"));
+        assertThat(proxiedHello.sayThankYou("Toby"), is("THANK YOU TOBY"));
+    }
+
+    static class UppercaseAdvice implements MethodInterceptor {
+        @Override
+        public Object invoke(MethodInvocation invocation) throws Throwable {
+            // 리플렉션의 Method와 달리 메소드 실행 시 타깃 오브젝트를 전달할 필요가 없음
+            // MethodInvocation 은 메소드 정보와 함께 타깃 오브젝트를 알고 있기 때문
+            String ret = (String) invocation.proceed();
+            return ret.toUpperCase();
+        }
+    }
+}
